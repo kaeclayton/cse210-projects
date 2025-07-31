f77b45a9be250848c0036de7c50886f1a3fcc747
kaeclayton <kclayton@byupathway.edu>
2025-07-30 22:43:11 -0700

    Homework for week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Kae Clayton", "Inheritance");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Caleb Clayton", "Triangles", "2.5", "10-15, 21-30");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Brad Clayton", "Gaming", "The Gamer's Guide to Fun");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
